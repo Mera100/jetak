a7d923c36dee70744f5d2143c86d4d8785a27523
majdhasan <majd.hasan@hotmail.com>
2021-08-01 11:52:49 +0200

    initial commit

---
+++ src/main/java/life/majd/jetak/jetak/JetakApplication.java
@@ -0,0 +1,13 @@
+package life.majd.jetak.jetak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/life/majd/jetak/jetak/JetakApplicationTests.java
@@ -0,0 +1,13 @@
+package life.majd.jetak.jetak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
